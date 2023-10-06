81a622a2545b4ab063a9dfd24b1a012637283f61
corlov <corlov@gmail.com>
2023-10-06 21:15:30 +0300

    Написал безопасный от скл-инбекций вариант запроса к БД

--- rest_service/main.go
+++ rest_service/main.go
@@ -53,42 +53,67 @@ func main() {
 }
 
 
+
+func NewNullString(s string) sql.NullString {
+    if len(s) == 0 {
+        return sql.NullString{}
+    }
+    return sql.NullString{
+         String: s,
+         Valid: true,
+    }
+}
+
 func getPersons(c *gin.Context) {
 	blockSize := 10
 	page := 0
 
-	sqlWhere := " (1 = 1) "
+	var (
+		argName string
+		argSurname string
+		argPatronymic string
+		argAge int = 0
+		argGender string
+		argNationality string
+	)
 
 	paramPairs := c.Request.URL.Query()
     for key, val := range paramPairs {
-        fmt.Printf("key: %v, value: %v\n", key, val)
 		switch key {
-			case "age":
-				ageVal, err := strconv.Atoi(string(val[0]))
-				if (err != nil) {
-					c.IndentedJSON(http.StatusNotFound, gin.H{"message": "error"})
-					return 
-				}
-				sqlWhere += "and (age = " + strconv.Itoa(ageVal) + ") "	
-
-				// FIXME: защита от инъекции SQL
 			case "name":
-				sqlWhere += "and (name = '" + val[0] + "') "
-			
+				argName = val[0]			
 			case "surname":
-				sqlWhere += "and (surname = '" + val[0] + "') "	
-
+				argName = val[0]
+			case "patronymic":
+				argPatronymic = val[0]
+			case "age":
+				var err error
+				argAge, err = strconv.Atoi(val[0])
+				fmt.Println("AGE: ", argAge)
+				if err != nil {
+					c.IndentedJSON(http.StatusInternalServerError, gin.H{"message": err.Error()})
+					return
+				}
+			case "gender":
+				argGender = val[0]
+			case "nationality":
+				argNationality = val[0]
 			case "page":
 				pageVal, _ := strconv.Atoi(string(val[0]))
 				page = 	blockSize * pageVal
 		}
     }
 
-	// rows2, err2 := db.Query("SELECT * FROM user WHERE id = ?", id)
+	sqlWhere := `
+		(name = $1       or NULLIF($1, '') is null) and
+		(surname = $2    or NULLIF($2, '') is null) and
+		(patronymic = $3 or NULLIF($3, '') is null) and
+		(age = $4        or NULLIF($4,  0) is null) and
+		(gender_id = $5  or NULLIF($5, '') is null) and
+		(country_id = $6 or NULLIF($6, '') is null)`
 
 	sqlLimit := " limit " + strconv.Itoa(blockSize)
 	sqlOffset := " offset " + strconv.Itoa(page)
-
 	
 	sqlQueryText := `
 		select  
@@ -101,18 +126,23 @@ func getPersons(c *gin.Context) {
 		from "Population".Person where ` + sqlWhere + sqlLimit + sqlOffset
 
 
-	fmt.Println(sqlQueryText)
+	// fmt.Println(sqlQueryText)
 	
 	connStr := fmt.Sprintf("host=%s port=%d user=%s "+ "password=%s dbname=%s sslmode=disable",
     host, port, user, password, dbname)
 	db, err := sql.Open("postgres", connStr)
     if err != nil {
 		c.IndentedJSON(http.StatusInternalServerError, gin.H{"message": err.Error()})
+		return
     }
     defer db.Close()
-    rows, err := db.Query(sqlQueryText)
+    
+	// Avoiding SQL injection risk
+	rows, err := db.Query(sqlQueryText, argName, argSurname, argPatronymic, argAge, argGender, argNationality)
+
     if err != nil {
         c.IndentedJSON(http.StatusInternalServerError, gin.H{"message": err.Error()})
+		return
     }
     defer rows.Close()
 
@@ -122,13 +152,14 @@ func getPersons(c *gin.Context) {
         err := rows.Scan(&p.Name, &p.Surname, &p.Patronymic, &p.Age, &p.Gender, &p.Nationality)
         if err != nil {
             c.IndentedJSON(http.StatusInternalServerError, gin.H{"message": err.Error()})
+			return
         }
-        fmt.Println("\n", p)
 		persons = append(persons, p)
     }
     err = rows.Err()
     if err != nil {
         c.IndentedJSON(http.StatusInternalServerError, gin.H{"message": err.Error()})
+		return
     }
 
 	c.IndentedJSON(http.StatusOK,  persons)   
